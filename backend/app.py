cf8e116c88450e9dd8eb263c6ed7bedbac3613a3
FlorFonseca <florencia.fonseca@correo.ucu.edu.uy>
2025-06-17 17:45:43 -0300

    arreglos

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,14 @@
 from flask import Flask
+from flask_cors import CORS
 import mysql.connector
 import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def home():
@@ -10,6 +16,7 @@ def home():
 
 @app.route('/dbcheck')
 def dbcheck():
+
     try:
         conn = mysql.connector.connect(
             host=os.getenv("DATABASE_HOST"),
@@ -21,3 +28,4 @@ def dbcheck():
         return "Conexión a MySQL exitosa!"
     except Exception as e:
         return f"Error de conexión: {str(e)}"
+
