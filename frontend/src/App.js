4f8dfa4a40a37a10eb0ed54c9f8c9ad40c9340f3
Martina Laventure <martina.laventure@correo.ucu.edu.uy>
2025-06-30 19:26:00 -0300

    algun cambio

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginVotante from './Views/LoginVotante';
 
 function App() {
   return (
-    <div>
-      <LoginVotante />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login/votante" element={<LoginVotante />} />
+        <Route path="/" element={<h2>Bienvenido a la app electoral</h2>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
